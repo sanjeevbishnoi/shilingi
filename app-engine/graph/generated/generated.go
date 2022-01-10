f16a9bb8d284ae99fca573d2faaf9af7d7a83828
Jack Kinga <jackmwangi@gmail.com>
2022-01-10 12:18:50 +0300

    update go generate

--- app-engine/graph/generated/generated.go
+++ app-engine/graph/generated/generated.go
@@ -52,6 +52,7 @@ type ComplexityRoot struct {
 	Item struct {
 		ID   func(childComplexity int) int
 		Name func(childComplexity int) int
+		Slug func(childComplexity int) int
 	}
 
 	Mutation struct {
@@ -130,6 +131,13 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.Item.Name(childComplexity), true
+
+	case "Item.slug":
+		if e.complexity.Item.Slug == nil {
+			break
+		}
+
+		return e.complexity.Item.Slug(childComplexity), true
 
 	case "Mutation.createItem":
 		if e.complexity.Mutation.CreateItem == nil {
@@ -348,6 +356,7 @@ var sources = []*ast.Source{
 	{Name: "graph/models.graphqls", Input: `type Item implements Node {
   id: ID!
   name: String!
+  slug: String!
 }
 
 input ItemInput {
@@ -560,6 +569,41 @@ func (ec *executionContext) _Item_name(ctx context.Context, field graphql.Collec
 	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
 		ctx = rctx // use context from middleware stack in children
 		return obj.Name, nil
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(string)
+	fc.Result = res
+	return ec.marshalNString2string(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) _Item_slug(ctx context.Context, field graphql.CollectedField, obj *ent.Item) (ret graphql.Marshaler) {
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	fc := &graphql.FieldContext{
+		Object:     "Item",
+		Field:      field,
+		Args:       nil,
+		IsMethod:   false,
+		IsResolver: false,
+	}
+
+	ctx = graphql.WithFieldContext(ctx, fc)
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return obj.Slug, nil
 	})
 	if err != nil {
 		ec.Error(ctx, err)
@@ -2674,6 +2718,11 @@ func (ec *executionContext) _Item(ctx context.Context, sel ast.SelectionSet, obj
 			}
 		case "name":
 			out.Values[i] = ec._Item_name(ctx, field, obj)
+			if out.Values[i] == graphql.Null {
+				invalids++
+			}
+		case "slug":
+			out.Values[i] = ec._Item_slug(ctx, field, obj)
 			if out.Values[i] == graphql.Null {
 				invalids++
 			}
