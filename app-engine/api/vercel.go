05007a558d3d1573a54cea79f9d418eff855ed60
Jack Kinga <jackmwangi@gmail.com>
2022-01-30 10:00:57 +0300

    edit db connection string

--- app-engine/api/vercel.go
+++ app-engine/api/vercel.go
@@ -24,7 +24,7 @@ func Shilingi(w http.ResponseWriter, r *http.Request) {
 	dbPass := os.Getenv("PLANETSCALE_DB_PASSWORD")
 	dbHost := os.Getenv("PLANETSCALE_DB_HOST")
 	db := os.Getenv("PLANETSCALE_DB")
-	fmt.Println("URI", fmt.Sprintf("mysql://%s:%s@tcp(%s)/%s",
+	fmt.Println("URI", fmt.Sprintf("mysql://%s:%s@tcp(%s)/%s?sslaccept=strict&tls=skip-verify",
 		dbUsername, dbPass, dbHost, db))
 	cli, err := ent.Open("mysql",
 		fmt.Sprintf("mysql://%s:%s@tcp(%s)/%s",
