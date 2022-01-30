aad77ab703023c8761067d3238a756dccf71b868
Jack Kinga <jackmwangi@gmail.com>
2022-01-30 08:10:24 +0300

    remove env debug code

--- app-engine/api/vercel.go
+++ app-engine/api/vercel.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"entgo.io/contrib/entgql"
 	"github.com/99designs/gqlgen/graphql"
@@ -22,7 +21,7 @@ import (
 // Shilingi serves as a Serveless Entrypoint for vercel
 func Shilingi(w http.ResponseWriter, r *http.Request) {
 	cfg := config.SetupConfig()
-	log.Printf("Configs: %+v,, Envs: %s", cfg, os.Environ())
+	log.Printf("Configs: %+v,", cfg)
 
 	cli, err := ent.Open("mysql", cfg.PlanetScaleURI())
 	if err != nil {
