81cab4479a720db2daa0ddf43fd6f5e4603f714b
Jack Kinga <jackmwangi@gmail.com>
2022-02-15 21:11:26 +0300

    do cleaning for name of tag

--- app-engine/ent/schema/tag.go
+++ app-engine/ent/schema/tag.go
@@ -1,11 +1,17 @@
 package schema
 
 import (
+	"context"
+	"regexp"
+	"strings"
+
 	"entgo.io/ent"
-	"entgo.io/ent/entc/integration/json/ent/hook"
 	"entgo.io/ent/schema/field"
 	"entgo.io/ent/schema/index"
 	"entgo.io/ent/schema/mixin"
+
+	gen "github.com/kingzbauer/shilingi/app-engine/ent"
+	"github.com/kingzbauer/shilingi/app-engine/ent/hook"
 )
 
 // Tag holds the schema definition for the Tag entity.
@@ -40,12 +46,26 @@ func (Tag) Indexes() []ent.Index {
 	}
 }
 
+var rmSpecialChars = regexp.MustCompile(`[^\w\s]`)
+var extraSpaces = regexp.MustCompile(`\s+`)
+
 // Hooks of the Tag
 func (Tag) Hooks() []ent.Hook {
 	return []ent.Hook{
 		hook.On(
-			func(m ent.Mutator) ent.Mutator {
-				return nil
+			func(next ent.Mutator) ent.Mutator {
+				return hook.TagFunc(func(ctx context.Context, m *gen.TagMutation) (ent.Value, error) {
+					// We will do a bit of cleaning for the tag name
+					if name, exists := m.Name(); exists {
+						// remove special
+						name = rmSpecialChars.ReplaceAllLiteralString(name, "")
+						name = extraSpaces.ReplaceAllLiteralString(name, " ")
+						name = strings.ToLower(name)
+						m.SetName(name)
+					}
+
+					return next.Mutate(ctx, m)
+				})
 			},
 			ent.OpCreate|ent.OpUpdate|ent.OpUpdateOne,
 		),
