4bf992a4f6198d85377497e4401006bd49fb78dd
Jack Kinga <jackmwangi@gmail.com>
2022-03-29 16:33:39 +0300

    add sub-label definition

---
+++ app-engine/ent/schema/sublabel.go
@@ -0,0 +1,47 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	"entgo.io/ent/schema/mixin"
+)
+
+// SubLabel holds the schema definition for the SubLabel entity.
+type SubLabel struct {
+	ent.Schema
+}
+
+// Fields of the SubLabel.
+func (SubLabel) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name").
+			Comment("Label name"),
+	}
+}
+
+// Edges of the SubLabel.
+func (SubLabel) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("parent", Tag.Type).
+			Ref("children").
+			Unique(),
+	}
+}
+
+// Mixin of the SubLabel
+func (SubLabel) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.Time{},
+	}
+}
+
+// Indexes of the SubLabel
+func (SubLabel) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("name").
+			Edges("parent").
+			Unique(),
+	}
+}
