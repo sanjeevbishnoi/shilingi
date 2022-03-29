8639989379f2f368ec13c2be1a9df4d20f3b7a3f
Jack Kinga <jackmwangi@gmail.com>
2022-03-29 16:54:42 +0300

    define mutation

--- app-engine/ent/schema/sublabel.go
+++ app-engine/ent/schema/sublabel.go
@@ -17,7 +17,8 @@ type SubLabel struct {
 func (SubLabel) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("name").
-			Comment("Label name"),
+			Comment("Label name").
+			StructTag(`conform:"trim"`),
 	}
 }
 
