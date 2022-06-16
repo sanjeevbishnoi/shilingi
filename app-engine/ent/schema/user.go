e18ea7f6dcd191fa7035a175bdfc5f7e8156a53d
Jack Kinga <jackmwangi@gmail.com>
2022-06-16 09:09:02 +0300

    add a user entity

---
+++ app-engine/ent/schema/user.go
@@ -0,0 +1,45 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("external_id").
+			Comment("If this user is from a different external system").
+			Optional().
+			Unique(),
+		field.String("first_name").
+			Optional(),
+		field.String("other_names").
+			Optional(),
+		field.String("email").
+			Comment("user's primary communication email").
+			Unique(),
+		field.Bool("is_email_verified"),
+		field.Enum("external_source").
+			Values("FIREBASE").
+			Default("FIREBASE"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
+
+// Mixin of the User
+func (User) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.Time{},
+	}
+}
